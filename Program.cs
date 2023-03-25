ca828dd0cd21eeb993d25ae8d651146c28747b1e
bobs3400 <124318533+bobs3400@users.noreply.github.com>
2023-03-26 09:43:17 +1000

    01

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿    Console.WriteLine("Введите длину массива");
+    int n = int.Parse(Console.ReadLine());
+//    static void Main(string[] args)
+//    {
+//        string mass;
+//        int n;
+//        int count;
+//        
+//        count = 0;
+//        while (count < n)
+//        {
+//            mass[count] = Console.ReadLine()
+//            count = count + 1;
+//        }
+//        string arr;
+//        
+//        count = 0;
+//       i = 0;
+//        while (count < n)
+//        {
+//            if ("")
+//            {
+//                arr[i] = mass[count];
+//                i = i + 1;
+//            }
+//            count = count + 1;
+//        }
+//        Console.WriteLine(arr);
+//    }
+    
+    // .NET can only read single characters or entire lines from the
+    // console. The following function safely reads a double value.
+//    private static double inputValue()
+//    {
+//        double result;
+//        while (!double.TryParse(Console.ReadLine(), out result));
+//        return result;
+//    }
+//}
